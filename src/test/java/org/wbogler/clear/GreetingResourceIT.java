6558e41fc6b79dd1dc60324e9937aa8df7626aa7
Willian Bogler <willian.silva@pti.org.br>
2024-04-12 09:26:43 -0300

    abertura

---
+++ src/test/java/org/wbogler/clear/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.wbogler.clear;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
